0effa886994ec799a7558e705af4db62b26375fe
aleksandrazt <s.tsarkova2013@yandex.ru>
2024-05-04 14:55:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+"""Небольшой модуль для напоминания делать перерыв при работе за компьютером"""
+import time
+from plyer import notification
+
+
+class EyesSaver:
+    """
+    Класс, который отвечает за вывод сообщения каждый 30 минут
+    """
+
+    def __init__(self):
+        # 240 = 30 минут за компьютером + 10 перерыва * 60 секунд
+        self.__time_for_wait = 240
+        self.__message = "Прошло пол часа и пора размять глаза"
+        self.__title = "Помни о глазах"
+
+    def wait_time(self):
+        """
+        Отсчитывает 30 минут перед тем как сделать перерыв
+        """
+        time.sleep(self.__time_for_wait)
+
+    def show_message(self):
+        """
+        Выводит уведомление о том, что надо сделать перерыв
+        """
+        notification.notify(message=self.__message, title=self.__title)
+
+    def work(self):
+        """
+        Фасад для бесконечной работы в пассивном режиме
+        """
+        while True:
+            self.wait_time()
+            self.show_message()
+
+
+if __name__ == '__main__':
+    eyes_saver = EyesSaver()
+    eyes_saver.work()
